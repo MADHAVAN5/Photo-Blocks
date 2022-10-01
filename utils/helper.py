efb78853181e5c57e829c59e76f08257403444a2
M.PRAVEEN KUMAR <mpkpraveen77o@gmail.com>
2022-10-01 12:28:41 +0530

    'smart_contract_development'

--- utils/helper.py
+++ utils/helper.py
@@ -11,9 +11,25 @@ from algosdk import account, mnemonic, constants
 from algosdk.v2client import algod
 from algosdk.future import transaction
 import algosdk
+from algosdk.v2client import algod
+from algosdk import account as algo_acc
+import yaml
+import os
+from pathlib import Path
+from algosdk import mnemonic
+from algosdk.v2client import indexer
 
-algorand_token = 'hwfo8uf8ahfuoawhr82rh2woirfhaw89rfhu2roy89awfho2aero82ayr9roaw8fh'
-algorand_test_ip_address = 'https://testnet-api.algonode.cloud'
+
+def get_project_root_path() -> Path:
+    path = Path(os.path.dirname(__file__))
+    return path.parent.parent
+
+def load_config():
+    root_path = get_project_root_path()
+    config_location = os.path.join(root_path, 'config.yml')
+
+    with open(config_location) as file:
+        return yaml.full_load(file)
 
 def get_db_connection():
     """
@@ -36,80 +52,3 @@ def get_mnemonic(private_key: str) -> str:
     """
     the_mnemonic = mnemonic.from_private_key(private_key)
     return the_mnemonic
-
-def make_transaction(private_key, sender_address, receiver_address, amount_to_send):
-    """
-    A function to make a transaction between two accounts
-    """
-    # connect with a client given the token and address of the network
-    algod_client = algod.AlgodClient(algorand_token, algorand_test_ip_address)
-
-    # print basic sender info
-    print("\nSender address: {}".format(sender_address))
-    account_info = algod_client.account_info(sender_address)
-    print("Account balance: {} microAlgos".format(account_info.get('amount')))
-
-    # build transaction
-    params = algod_client.suggested_params()
-    # comment out the next two (2) lines to use suggested fees
-    params.flat_fee = constants.MIN_TXN_FEE
-    params.fee = 1000
-    # amount = 100000
-    amount = amount_to_send
-    note = "Initial transaction example".encode()
-
-    # print basic receiver info
-    print("\nReceiver address: {}".format(receiver_address))
-    account_info = algod_client.account_info(receiver_address)
-    print("Account balance: {} microAlgos".format(account_info.get('amount')))
-
-    unsigned_txn = transaction.PaymentTxn(sender_address, params, receiver_address, amount, None, note)
-
-    # sign transaction
-    signed_txn = unsigned_txn.sign(private_key)
-
-    # submit transaction
-    tx_id = algod_client.send_transaction(signed_txn)
-    print("Signed transaction with tx_id: {}".format(tx_id))
-
-    # wait for confirmation
-    try:
-        confirmed_txn = transaction.wait_for_confirmation(algod_client, tx_id, 4)
-    except Exception as err:
-        print("A damn error occurred: {}".format(err))
-        return 
-
-    print("Transaction information: {}".format(
-        json.dumps(confirmed_txn, indent=4)))
-    print("Decoded note: {}".format(base64.b64decode(
-        confirmed_txn["txn"]["txn"]["note"]).decode()))
-
-    print("Starting Account balance: {} microAlgos".format(
-        account_info.get('amount')))
-    print("Amount transferred: {} microAlgos".format(amount))
-    print("Fee: {} microAlgos".format(params.fee))
-
-    account_info = algod_client.account_info(sender_address)
-    print("Final Account balance: {} microAlgos".format(
-        account_info.get('amount')) + "\n")
-    return True
-
-def get_balance(account_address: str) -> int:
-    """
-    A function that tells the accounts balance given an account address
-    """
-    # create a client to interact with
-    client = algod.AlgodClient(algorand_token, algorand_test_ip_address)
-    account_info = client.account_info(account_address)
-    print(json.dumps(account_info, indent=4))
-
-    account_balance = account_info.get('amount')
-    print('Account balance: {} microAlgos'.format(account_balance))
-    return account_balance
-
-def get_records(account_address):
-    """
-    A function to get all account related records
-    """
-    print(account_address)
-    return 1
---
+++ utils/service/marketplace_interfaces/nft_marketplace.py
@@ -0,0 +1,113 @@
+from abc import ABC, abstractmethod
+from pyteal import *
+import algosdk
+
+from marketplace_interfaces.nft_marketplace import NFTMarketplaceInterface
+
+class NFTMarketplaceInterface(ABC):
+
+    @abstractmethod
+    def initialize_escrow(self, escrow_address):
+        curr_escrow_address = App.globalGetEx(Int(0), self.Variables.escrow_address) 
+
+        asset_escrow = AssetParam.clawback(Txn.assets[0])
+        manager_address = AssetParam.manager(Txn.assets[0])
+        freeze_address = AssetParam.freeze(Txn.assets[0])
+        reserve_address = AssetParam.reserve(Txn.assets[0])
+        default_frozen = AssetParam.defaultFrozen(Txn.assets[0])
+
+        return Seq([
+            curr_escrow_address,
+            Assert(curr_escrow_address.hasValue() == Int(0)), 
+
+            Assert(App.globalGet(self.Variables.app_admin) == Txn.sender()),
+            Assert(Global.group_size() == Int(1)),
+
+            asset_escrow,
+            manager_address,
+            freeze_address,
+            reserve_address,
+            default_frozen,
+            Assert(Txn.assets[0] == App.globalGet(self.Variables.asa_id)),
+            Assert(asset_escrow.value() == Txn.application_args[1]),
+            Assert(default_frozen.value()),
+            Assert(manager_address.value() == Global.zero_address()),
+            Assert(freeze_address.value() == Global.zero_address()),
+            Assert(reserve_address.value() == Global.zero_address()),
+
+            App.globalPut(self.Variables.escrow_address, escrow_address),
+            App.globalPut(self.Variables.app_state, self.AppState.active),
+            Return(Int(1))
+        ])
+
+    @abstractmethod
+    def make_sell_offer(self, sell_price):
+        valid_number_of_transactions = Global.group_size() == Int(1)
+        app_is_active = Or(App.globalGet(self.Variables.app_state) == self.AppState.active,
+                           App.globalGet(self.Variables.app_state) == self.AppState.selling_in_progress)
+
+        valid_seller = Txn.sender() == App.globalGet(self.Variables.asa_owner)
+        valid_number_of_arguments = Txn.application_args.length() == Int(2)
+
+        can_sell = And(valid_number_of_transactions,
+                       app_is_active,
+                       valid_seller,
+                       valid_number_of_arguments)
+
+        update_state = Seq([
+            App.globalPut(self.Variables.asa_price, Btoi(sell_price)),
+            App.globalPut(self.Variables.app_state, self.AppState.selling_in_progress),
+            Return(Int(1))
+        ])
+
+        return If(can_sell).Then(update_state).Else(Return(Int(0)))
+
+    @abstractmethod
+    def buy(self):
+        valid_number_of_transactions = Global.group_size() == Int(3)
+        asa_is_on_sale = App.globalGet(self.Variables.app_state) == self.AppState.selling_in_progress
+
+        valid_payment_to_seller = And(
+            Gtxn[1].type_enum() == TxnType.Payment,
+            Gtxn[1].receiver() == App.globalGet(self.Variables.asa_owner), # correct receiver
+            Gtxn[1].amount() == App.globalGet(self.Variables.asa_price), # correct amount 
+            Gtxn[1].sender() == Gtxn[0].sender(), # equal sender of the first two transactions, which is the buyer
+            Gtxn[1].sender() == Gtxn[2].asset_receiver() # correct receiver of the NFT
+        )
+
+        valid_asa_transfer_from_escrow_to_buyer = And(
+            Gtxn[2].type_enum() == TxnType.AssetTransfer,
+            Gtxn[2].sender() == App.globalGet(self.Variables.escrow_address),
+            Gtxn[2].xfer_asset() == App.globalGet(self.Variables.asa_id),
+            Gtxn[2].asset_amount() == Int(1)
+        )
+
+        can_buy = And(valid_number_of_transactions,
+                      asa_is_on_sale,
+                      valid_payment_to_seller,
+                      valid_asa_transfer_from_escrow_to_buyer)
+
+        update_state = Seq([
+            App.globalPut(self.Variables.asa_owner, Gtxn[0].sender()), # update the owner of the ASA.
+            App.globalPut(self.Variables.app_state, self.AppState.active), # update the app state
+            Return(Int(1))
+        ])
+
+        return If(can_buy).Then(update_state).Else(Return(Int(0)))
+
+    @abstractmethod
+    def stop_sell_offer(self):
+        valid_number_of_transactions = Global.group_size() == Int(1)
+        valid_caller = Txn.sender() == App.globalGet(self.Variables.asa_owner)
+        app_is_initialized = App.globalGet(self.Variables.app_state) != self.AppState.not_initialized
+
+        can_stop_selling = And(valid_number_of_transactions,
+                               valid_caller,
+                               app_is_initialized)
+
+        update_state = Seq([
+            App.globalPut(self.Variables.app_state, self.AppState.active),
+            Return(Int(1))
+        ])
+
+        return If(can_stop_selling).Then(update_state).Else(Return(Int(0)))
